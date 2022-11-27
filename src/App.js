5825d59248647e531141934343a6d584608fd418
Darshit <darshitmistry2@gmail.com>
2022-11-27 22:53:28 +0530

    Card section was updeted

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import {Card1, Card2, Card3, Email, Footer, Navbar} from './components'
 const App = () => {
   return (
     <>
-    <div>
       <Navbar/>
-    </div>
-    <div>
       <Card1/>
       <Card2/>
       <Card3/>
       <Email/>
-    </div>
     <div>
       <Footer/>
     </div>
--- src/components/card1/Card1.jsx
+++ src/components/card1/Card1.jsx
@@ -5,8 +5,36 @@ import  cardImg  from './import'
 const Card1 = () => 
 (
   <>
-  <div>
-    Card1
+  <div className="main_container">
+  <div className='__card1_container'>
+    <div className="__card1_container-text">
+        <h1 className="__card1_continer-text_main">
+        GET YOUR 
+        <br />
+        BUS PASS 
+        <br />
+        ONLINE
+        </h1>
+        <p className="__card1_container-text_sub">
+        It is easy to use interface for people 
+        <br/>
+        can take advantages of our system and save lots 
+        <br/>
+        of time and effort
+        </p>
+        <p className="__card1_container-text_sub2">
+        So get your pass now
+        </p>
+        <div className="__card1_container-button">
+          <button className="__card1_container-button_start"><a href="#">Let’s Go &#8594;</a></button>
+        </div>
+    </div>
+    <div className="__card1_container-img">
+      <div className="__card1_red"/>
+      <div className="__card1_container-card">
+      </div>
+  </div>
+  </div>
   </div>
   </>
 )
