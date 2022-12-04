cf64a68917eb942f4409a4e66bb8e0928ceb843c
Darshit <darshitmistry2@gmail.com>
2022-12-04 15:41:29 +0530

    Email Section was Updated

--- src/components/email/Email.jsx
+++ src/components/email/Email.jsx
@@ -3,8 +3,21 @@ import './email.css'
 
 const Email = () => {
   return (
-    <div>
-      Email
+    <div className="__Email_continer">
+      <div className="__Email_subcontainer">
+      <div className="__Email_text-main">
+        <p>
+        Let’s try our service now!
+        </p>
+        <p className='__Email_text-sub'>
+        Everything you need to accept card pyment and grow your <br />
+        business anywhere
+        </p>
+      </div>
+      <div className="__Email_button">
+       <button className="__Email_button-iner">Get start</button>
+      </div>
+      </div>
     </div>
   )
 }
