85a8fa101f5fbb1de12d173e97fdf8733bb5654c
Darshit <darshitmistry2@gmail.com>
2022-12-05 22:25:07 +0530

    New Navbar was Added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import {logo} from "./import"
+import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <>
+      <div className="_nav-container">
+        <div className="_nav-logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="_nav-link">
+          <span className='_link-home'><a href="">HOME</a></span>
+          <span className='_link-reg'><a href="">USER RAGISTRETION</a></span>
+          <span className='_link-about'><a href="">ABOUT US</a></span>
+          <span className='_link-cont'><a href="">CONTECT US</a></span>
+        </div>
+        <div className="_nav-log">
+          <a href="">LOGIN</a>
+        </div>
+      </div>
+    </>
   )
 }
 
