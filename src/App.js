e06722fa51b6534131a5bd7e07cd41e495172740
Darshit <darshitmistry2@gmail.com>
2022-12-08 13:06:50 +0530

    Some Animations was added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import './App.css'
 import {Card1, Card2, Card3, Email, Footer, Navbar,Info,Step} from './components'
+
 
 const App = () => {
   return (
